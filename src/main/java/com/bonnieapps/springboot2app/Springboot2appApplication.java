6eb2c6248103d1f5a8030f7714676e05e918f70f
bonfaceotieno <bonface.otieno@systechafrica.com>
2023-02-04 17:15:44 +0300

    new files added

---
+++ src/main/java/com/bonnieapps/springboot2app/Springboot2appApplication.java
@@ -0,0 +1,13 @@
+package com.bonnieapps.springboot2app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2appApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bonnieapps/springboot2app/Springboot2appApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonnieapps.springboot2app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2appApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
